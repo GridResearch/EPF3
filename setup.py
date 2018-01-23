eff12e6b32ac76bce09e160e6921b04ce6bebcaa
atwalsh <adamtwalsh@gmail.com>
2018-01-22 21:15:32 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -5,17 +5,13 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
 # To use a consistent encoding
-from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+here = path.abspath(path.dirname(__file__))
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
