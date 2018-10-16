fa9d196207148e218524bc58e754d6316715f304
Adam Walsh <adamtwalsh@gmail.com>
2018-10-16 12:00:26 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -36,16 +36,16 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',  # Required
+    version='0.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A Python 3 library for parsing and processing iTunes EPF files.',  # Required
+    description='A Python 3 library for parsing and processing iTunes EPF files.',
 
     # This should be your name or the name of the organization which owns the
     # project.
-    author='High Summit LLC',  # Optional
+    author='Grid Research, LLC',  # Optional
 
     # Classifiers help users find your project by categorizing it.
     #
